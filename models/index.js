53c97ac8d02e261b3f41d3529c5d37ad0f29485a
root <root@DESKTOP-RAA4UCT>
2023-07-15 13:04:35 +0530

    --

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
+//hi
 fs
   .readdirSync(__dirname)
   .filter(file => {
