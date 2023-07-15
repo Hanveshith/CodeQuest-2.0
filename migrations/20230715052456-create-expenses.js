ff20dd4591216f992f7b38c72958ca1bcf23907c
root <root@DESKTOP-RAA4UCT>
2023-07-15 11:28:46 +0530

    initial commit

---
+++ migrations/20230715052456-create-expenses.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Expenses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Datetime: {
+        type: Sequelize.DATE
+      },
+      ExpenseHead: {
+        type: Sequelize.STRING
+      },
+      Amount: {
+        type: Sequelize.INTEGER
+      },
+      Description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Expenses');
+  }
+};
